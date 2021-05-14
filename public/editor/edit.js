c22c76bff22f5c447522c1c1a5c94747a66fd033
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2021-05-14 03:22:01 -0300

    editor!

---
+++ public/editor/edit.js
@@ -0,0 +1,153 @@
+// edit.js
+// borges 2021 -- nenhum direito reservado
+// script da página de edição
+
+// todos os elementos contendo os formulários vão ficar aqui...
+let fields = [];
+
+// contador global para gerar IDs para os mini-forms...
+let incr = 0;
+
+// armazenando o objeto no escopo global de novo...
+let g = null;
+
+// gera um select de disciplinas
+function gen_sel_disc(gg) {
+  const code_select = document.createElement("select");
+  for (const cod in gg["detalhes"]) {
+    const option = document.createElement("option");
+    option.value = cod;
+    const det = gg["detalhes"][cod];
+    option.innerText = `${det["codigo"]} - ${det["nome"]}`;
+    code_select.appendChild(option);
+  }
+  return code_select;
+}
+
+// gera um select de dias da semana
+function gen_sel_day(gg) {
+  const day_select = document.createElement("select");
+  for (const k in DIAS["coloquial"]) {
+    const option = document.createElement("option");
+    option.value = k;
+    option.innerText = DIAS["coloquial"][k];
+    day_select.appendChild(option);
+  }
+  return day_select;
+}
+
+// gera um input de horário
+function gen_input_time() {
+  const inp = document.createElement("input");
+  inp.type = "time";
+  inp.pattern = "\\d{2}:\\d{2}";
+  inp.step = 60;
+  inp.value = "08:00";  
+  return inp;
+} 
+
+// insere um campo na lista!
+function add_field() {
+  const container = document.createElement("div");
+  const sel_disc = gen_sel_disc(g);
+  const sel_day = gen_sel_day(g);
+  const inp_start = gen_input_time();
+  const inp_end = gen_input_time();
+  const rem = document.createElement("button");
+  rem.text = "Remover";
+  container.appendChild(sel_disc);
+  container.appendChild(sel_day);
+  container.appendChild(inp_start);
+  container.appendChild(inp_end);
+  container.appendChild(rem);
+  container.className = "row";
+  rem.innerText = "Remover";
+  let obj = {
+    "container": container,
+    "sel_disc": sel_disc,
+    "sel_day": sel_day,
+    "inp_start": inp_start,
+    "inp_end": inp_end,
+    "rem": rem,
+    "id": incr
+  };
+  rem.addEventListener("click", remove_field_btn);
+  rem.setAttribute("incr", incr);
+  fields.push(obj);
+  document.getElementById("iaulas").appendChild(container);
+  incr++;
+  return obj;
+}
+
+// remove um campo da lista (chamado pelo botão)
+function remove_field(incremental_id) {
+  for (let index = 0; index < fields.length; index++) {
+    const field = fields[index];
+    if (field["id"] == incremental_id) {
+      fields.splice(index, 1);
+      field["container"].remove();
+      console.log(index, "removido", incremental_id)
+      return true;
+    }
+  }
+  throw "falha ao remover " + incremental_id;
+  return false;
+}
+
+// remove um campo da lista de acordo com o botão
+function remove_field_btn(evt) {
+  const btn = evt.target;
+  console.log(btn);
+  remove_field(btn.getAttribute("incr"));
+}
+
+
+// inicializa os campos para editar a brincadeira
+function init_fields() {
+  g = extract();
+  for (const aula of g["aulas"]) {
+    const field = add_field();
+    field["sel_disc"].value = aula["codigo"];
+    field["sel_day"].value = aula["dia"];
+    field["inp_start"].value = aula["inicio"];
+    field["inp_end"].value = aula["fim"];
+  }
+}
+
+// captura os valores dos campos num novo vetor aulas
+function capture_fields() {
+  let aulas = [];
+  for (const field of fields) {
+    aulas.push({
+      "codigo": field["sel_disc"].value,
+      "dia": field["sel_day"].value,
+      "inicio": field["inp_start"].value,
+      "fim": field["inp_end"].value
+    });
+  }
+  return aulas;
+}
+
+// ação de botão: reseta alterações
+function reset() {
+  let field;
+  while (field = fields.pop()) {
+    field["container"].remove();
+  }
+  init_fields();
+}
+
+// ação de botão: volta
+function givei() {
+  const o = new URLSearchParams(window.location.search).get("o");
+  window.location.assign("../?o=" + o);
+}
+
+// ação de botão: salva e volta
+function commit() {
+  g["aulas"] = capture_fields();
+  const o = compress(JSON.stringify(g));
+  window.location.assign("../?o=" + o);
+}
+
+init_fields();
--- public/view.js
+++ public/view.js
@@ -1,139 +1,15 @@
-// bookmarklet.js
+// view.js
 // borges 2021 - nenhum direito reservado
 // script principal da página
 
 // armazenado globalmente para não termos que descomprimir toda vez
 let g = null;
 
-// versão do objeto exigida, será descartado se diferir
-const obj_ver = 1;
-
-// cores padrão legaizinhas
-const COLORS = [
-  "#e74c3c", "#8e44ad", "#3498db", "#1abc9c", "#27ae60", "#f1c40f",
-  "#f39c12", "#ec7063", "#c39bd3", "#85c1e9", "#7dcea0", "#f8c471"
-];
-
-// nomes dos dias de semana nas várias configurações
-const DIAS = {
-  "letra": {
-    "Dom": "D",
-    "Seg": "S",
-    "Ter": "T",
-    "Qua": "Q",
-    "Qui": "Q",
-    "Sex": "S",
-    "Sab": "S"
-  },
-  "abreviado": {
-    "Dom": "Dom",
-    "Seg": "Seg",
-    "Ter": "Ter",
-    "Qua": "Qua",
-    "Qui": "Qui",
-    "Sex": "Sex",
-    "Sab": "Sáb"
-  },
-  "coloquial": {
-    "Dom": "Domingo",
-    "Seg": "Segunda",
-    "Ter": "Terça",
-    "Qua": "Quarta",
-    "Qui": "Quinta",
-    "Sex": "Sexta",
-    "Sab": "Sábado"
-  },
-  "completo": {
-    "Dom": "Domingo",
-    "Seg": "Segunda-feira",
-    "Ter": "Terça-feira",
-    "Qua": "Quarta-feira",
-    "Qui": "Quinta-feira",
-    "Sex": "Sexta-feira",
-    "Sab": "Sábado"
-  }
-}
-
 // nomes das opções
 const OPTS = [
   "prop", "sep", "evil", "fmt", "fday", "stra", "strw", "cmix", "alpha",
   "nosab"
 ];
-
-// função auxiliar: pega o valor de um input
-function input_get(elem) {
-  if (elem.type == "checkbox") {
-    return elem.checked;
-  } else if (elem.type == "range") {
-    if (elem.step == 1) {
-      return parseInt(elem.value);
-    } else {
-      return parseFloat(elem.value);
-    }
-  } else {
-    return elem.value;
-  }
-}
-
-// função auxiliar: seta o valor de um input
-function input_set(elem, val) {
-  if (elem.type == "checkbox") {
-    return elem.checked == val;
-  } else {
-    return elem.value = val;
-  }
-}
-
-// algoritmo de compressão LZString, minificado pra caber em ~1.5 kB
-// thanks, @pieroxy
-const _compress = function(o,r,n){if(null==o)return"";var e,t,i,s={},p={},u="",c="",a="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<o.length;i+=1)if(u=o.charAt(i),Object.prototype.hasOwnProperty.call(s,u)||(s[u]=f++,p[u]=!0),c=a+u,Object.prototype.hasOwnProperty.call(s,c))a=c;else{if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++),s[c]=f++,a=String(u)}if(""!==a){if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==r-1){d.push(n(m));break}v++}return d.join("")}
-const e = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$";
-function compress(o) {
-  return null==o?"":_compress(o,6,function(o){return e.charAt(o)})
-}
-
-// função auxiliar -- descomprime o JSON usando lz-string
-function decomp(s) {
-  return LZString.decompressFromEncodedURIComponent(s);
-}
-
-// insere elementos entre elementos de uma array
-const interleave = (a, e) => [].concat(...a.map(n => [n, e])).slice(0, -1);
-
-// retorna o comprimento, em bytes, de uma string
-function bytelen(s) {
-  return (new TextEncoder().encode(s)).length
-}
-
-// extrai o objeto da URL, e insere alguns detalhes adicionais
-function extract(dbg) {
-  const o = new URLSearchParams(window.location.search).get("o");
-  if (!o) return null;
-  const d = decomp(o);
-  const a = bytelen(o), b = bytelen(d);
-  if (dbg)
-    console.log(
-      `Compressão encolheu o JSON em ~${Math.round(100-100*a/b)}%!`,
-      `(de ${b} para ${a} bytes)`
-    );
-  let obj = JSON.parse(d);
-  if (obj["ver"] != obj_ver) {
-    alert("O bookmarklet foi atualizado! Você vai precisar rodar de novo...");
-    location.assign("./");
-  }
-  obj["dias"] = ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
-  let hors = [];
-  for (const aula of obj["aulas"]) {
-    const i = hor2int(aula["inicio"]);
-    const f = hor2int(aula["fim"]);
-    hors.push(i);
-    hors.push(f);
-  }
-  let nh = [...new Set(hors)];
-  nh.sort((a, b) => a - b);
-  obj["horarios"] = nh;
-  return obj;
-}
 
 // detecta a presença do parâmetro e esconde o div correspondente
 function hide_correct() {
@@ -184,11 +60,13 @@ function gen_colors() {
     inp.addEventListener("change", function() {
       regen();
     });
-    txt.addEventListener("click", function(e) {
+    txt.addEventListener("click", function(evt) {
       const newname = prompt("Novo nome:", nome);
-      if (newname) g["detalhes"][k]["nome"] = newname;
+      if (newname) {
+        evt.target.innerText = `${codigo} - ${newname}`;
+        g["detalhes"][k]["nome"] = newname;
+      }
       regen();
-      gen_colors();
     });
     tcol.appendChild(inp);
     tcol.appendChild(document.createElement("br"));
@@ -206,14 +84,6 @@ function color_inputs() {
     ins[cod] = document.getElementById("opt_color_" + cod);
   }
   return ins;
-}
-
-// função auxiliar: embaralha uma array
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
 }
 
 // embaralha a ordem das cores
@@ -287,38 +157,6 @@ function stripes(colors, angle, width) {
   return `${rlg}(\n  ${angle},\n  ${colors[0]},\n  ${cs.join(",\n  ")}\n)`;
 }
 
-// converte cor em hexa para rgb
-function hex2rgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  const obj = result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
-  if (!obj) throw `bad hex ${hex}`
-  return [obj.r, obj.g, obj.b];
-}
-
-// converte cor em hexa para rgba
-function hex2rgba(hex, alpha) {
-  let a = hex2rgb(hex);
-  a.push(alpha);
-  return a;
-}
-
-// converte cor em rgb para hexa
-function rgb2hex(r, g, b) {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-// escreve uma cor rgb em css
-function rgb2css(arr) {
-  if (arr.length == 3) return `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
-  else if (arr.length == 4)
-    return `rgba(${arr[0]}, ${arr[1]}, ${arr[2]}, ${arr[3]})`;
-  else throw `wtf kinda color is ${arr}?`;
-}
-
 // gera propriedade de fundo baseado em cores e opções
 function setbg(elem, colors, strategy, strw, stra, alpha) {
   if (strategy == "blend" || colors.length == 1) {
@@ -331,48 +169,6 @@ function setbg(elem, colors, strategy, strw, stra, alpha) {
   } else {
     throw `bad color strategy ${strategy}`;
   }
-}
-
-// converte horário XX:XX para minutos totais
-function hor2int(hor) {
-  const hre = /(\d{2}):(\d{2})/;
-  const match = hor.match(hre);
-  return parseInt(match[1])*60 + parseInt(match[2]);
-}
-
-// converte minutos totais de volta para horário XX:YY
-// "evil" deixa o horário no formato americano do mal: AM/PM sem zero antes
-function int2hor(mins, evil) {
-  let h = Math.floor(mins/60);
-  let m = Math.round(mins - h*60).toFixed(0);
-  if (m.length < 2) m = "0" + m;
-  if (evil) {
-    let a = "AM";
-    if (h > 12) {
-      h %= 12;
-      a = "PM";
-    }
-    return `${h.toFixed(0)}:${m} ${a}`;
-  } else {
-    h = h.toFixed(0);
-    if (h.length < 2) h = "0" + h;
-    return `${h}:${m}`;
-  }
-}
-
-// acha as aulas num certo dia e bloco
-function byblock(gg, day, s, e) {
-  let cods = [];
-  for (const aula of gg["aulas"]) {
-    const ad = aula["dia"];
-    const i = hor2int(aula["inicio"]);
-    const f = hor2int(aula["fim"]);
-    if (ad == day && i <= s && f >= e) {
-      cods.push(aula["codigo"]);
-    }
-  }
-  cods.sort();
-  return [...new Set(cods)];
 }
 
 // coloca o texto formatado num td
@@ -434,6 +230,12 @@ function obj2opt(obj) {
   for (const oname in obj) {
     input_set(document.getElementById(`opt_${oname}`), obj[oname]);
   }
+}
+
+// redireciona para a página de edição
+function edit() {
+  const o = compress(JSON.stringify(g));
+  window.location.assign("editor/?o=" + o);
 }
 
 // monta a tabela com o horário
